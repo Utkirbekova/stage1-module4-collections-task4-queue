ce724ac616532da7d0de9e2a87fac4208f0f66ec
Utkirbekova <otkirbekovamadinabonu@gmail.com>
2022-11-15 18:24:06 +0500

    .

--- src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
+++ src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
@@ -1,9 +1,19 @@
 package com.epam.collections.queue;
 
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class PriorityQueueCreator {
     public PriorityQueue<String> createPriorityQueue(List<String> firstList, List<String> secondList) {
+        List<String> list = new ArrayList<>();
+        list.addAll(firstList);
+        list.addAll(secondList);
+        Collections.sort(list);
+
+        Collections.reverse(list);
+
+        Comparator<String> comp = (o1, o2) -> o2.compareTo(o1);
+        PriorityQueue<String> priorityQueue = new PriorityQueue<>(comp);
+        priorityQueue.addAll(list);
+        return priorityQueue;
     }
 }
